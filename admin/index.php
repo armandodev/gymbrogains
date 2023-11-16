cd61d5f91bbd3ea4ccf021c6a4d9c663c73a1cc1
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2023-11-16 08:04:00 -0600

    Correcciones de codigo

--- admin/index.php
+++ admin/index.php
@@ -5,25 +5,6 @@ if (!$isAdmin && !$isLogged) {
   header("location:../index.php");
   exit();
 }
-
-// Aquí van a trabajar, harán la pagina de inicio de admin, y la pagina de administración de usuarios dentro de admin/users.php y admin/index.php. Principalmente trabajaran con la estructura HTML de ambas páginas, y con el manejo dinámico desde la base de datos. Les dare el acceso en breve
-
-/* Secciones
-  * Header
-    * Logo
-    * Navbar <- El nav bar lo encuentran en el index de la parte publica, lo pueden copiar y pegar
-    * Botón para mostrar el menu
-  * Main
-    * Banner con el nombre de la pagina
-    * Sección de usuarios
-      * Tabla con los 5 usuarios más recientes y un botón para ver todos los usuarios
-    * Sección de ejercicios
-      * Tabla con los 5 ejercicios más recientes y un botón para ver todos los ejercicios
-    * Sección de foro
-      * Tabla con los 5 temas más recientes y un botón para ver todos los temas
-  * Footer
-    * Leyenda de derechos de autor
-*/
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -31,17 +12,21 @@ if (!$isAdmin && !$isLogged) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Administración | Gymbrogains</title> <!-- Aquí va el titulo de la pagina -->
+  <title>Administración | Gymbrogains</title>
 
+  <link rel="stylesheet" href="./../fonts/css/index.css">
+  <link rel="stylesheet" href="./../css/normalize.css">
+  <link rel="stylesheet" href="./../css/global.css">
+  <link rel="stylesheet" href="./../css/header.css">
+  <link rel="stylesheet" href="./../css/footer.css">
 </head>
 
 <body>
-  <!-- Dentro del header ira el logo, la barra y el botón, esto esta en el index solo copia y pega -->
   <header>
     <div class="header-container">
       <div class="header-logo">
         <a href="./index.php">
-          <img src="./images/logo/white.webp" alt="Gymbrogains" />
+          <img src="./../images/logo/white.webp" alt="Gymbrogains" />
         </a>
       </div>
       <nav class="header-nav">
@@ -56,40 +41,18 @@ if (!$isAdmin && !$isLogged) {
           <li class="header-nav-link">
             <a href="./calculator.php">Calculadora</a>
           </li>
-          <?php
-          if (isset($_SESSION["user"])) { ?>
-            <li class="header-nav-link">
-              <a href="./forum.php">Foro</a>
-            </li>
-            <li class="header-nav-link">
-              <a href="./profile.php">
-                <span class="material-icons"> account_circle </span>
-                <span id="profile-nav"></span>
-              </a>
-            </li>
-            <?php if ($_SESSION['user']['Role'] == 0) { ?>
-              <li class="header-nav-link">
-                <a href="./admin/index.php">
-                  <span class="material-icons"> admin_panel_settings </span>
-                  <span id="admin-nav"></span>
-                </a>
-              </li>
-            <?php
-            }
-          } else { ?>
-            <li class="header-nav-link">
-              <a href="./login.php">
-                <span class="material-icons"> login </span>
-                <span id="login-nav"></span>
-              </a>
-            </li>
-            <li class="header-nav-link">
-              <a href="./signup.php">
-                <span class="material-icons"> person_add </span>
-                <span id="signup-nav"></span>
-              </a>
-            </li>
-          <?php } ?>
+          <li class="header-nav-link">
+            <a href="./forum.php">Foro</a>
+          </li>
+          <li class="header-nav-link">
+            <a href="./forum.php">Usuarios</a>
+          </li>
+          <li class="header-nav-link">
+            <a href="./../index.php">
+              <span class="material-icons"> public </span>
+              Publica
+            </a>
+          </li>
         </ul>
       </nav>
 
@@ -98,7 +61,7 @@ if (!$isAdmin && !$isLogged) {
       </button>
     </div>
   </header>
-  <!-- Dentro del main van secciones(section) y dentro de las secciones van artículos(article) -->
+
   <main>
     <section id="banner">
       <article>
@@ -111,7 +74,6 @@ if (!$isAdmin && !$isLogged) {
 
         <li class="">
           <div class="">
-
             <h2 class="">
               <?php echo $users['UserName']; ?>
             </h2>
@@ -170,12 +132,15 @@ if (!$isAdmin && !$isLogged) {
       </ul>
     </section>
   </main>
-  <!-- El footer nomas es una cosa no mamen, si mamo-->
+
   <footer>
     <div class="footer-bottom">
       <p>&copy; 2023 Gymbrogains - Todos los derechos reservados</p>
     </div>
   </footer>
+
+  <script src="./../js/jquery/jquery-3.7.1.min.js"></script>
+  <script src="./../js/header.js"></script>
 </body>
 
 </html>
--- includes/database.php
+++ includes/database.php
@@ -3,11 +3,7 @@ class Database
 {
   // Datos de configuración de la base de datos
   private $host = "localhost"; // Host de la base de datos
-<<<<<<< HEAD
-  private $database = "gymbrogains"; // Nombre de la base de datos
-=======
   private $database = "latest-gymbrogains-db"; // Nombre de la base de datos
->>>>>>> 3ab6380 (Connection)
   private $username = "root"; // Nombre de usuario de la base de datos
   private $password = ""; // Contraseña de la base de datos
 
